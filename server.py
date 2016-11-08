a61415012e2b265f286a90bf0f5a95d31ca0906f
Phi Nguyen <phi.nguyen@bstarsolutions.com>
2016-11-08 14:38:37 +0700

    - Minor change

--- server.py
+++ server.py
@@ -48,24 +48,6 @@ def incoming():
   resp.say("in! in! in! in! in! in! in! in! in! in! in! in! in! in! in! in! in! in! in! ")
   return str(resp)
 
-@app.route("/voice", methods=['POST'])
-def voice():
-    resp = twilio.twiml.Response()
-    from_client   = request.values.get('From')
-    to_client     = request.values.get('To')
-    if to_client != '':
-        dial = resp.dial(from_client)
-        # wrap the phone number or client name in the appropriate TwiML verb
-        # by checking if the number given has only digits and format symbols
-        if phone_pattern.match(to_client):
-            dial.number(to_client)
-        else:
-            dial.client(to_client)
-    else:
-        resp.say("Thanks for calling!")
-    
-    return Response(str(resp), mimetype='text/xml')
-
 @app.route('/placeCall', methods=['GET', 'POST'])
 def placeCall():
   account_sid = os.environ.get("ACCOUNT_SID", ACCOUNT_SID)
